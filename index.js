cfd1296a542f38df092c21f6487f569b0e12d291
hantoli <hantoli@amazon.com>
2022-03-29 17:39:16 -0300

    modified index.js message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/status', (req, res) => {
-  return res.send({ message: "Demo service is up"});
+  return res.send({ message: "Demo service is ready in ECS"});
 })
 
 app.listen(PORT, () => {
